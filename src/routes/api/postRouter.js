48b7cf1080d653b62e47f1ad5b3ec525c17e2292
M0ngi <saidanemongi@gmail.com>
2024-01-24 18:23:01 +0100

    fix

--- src/routes/api/postRouter.js
+++ src/routes/api/postRouter.js
@@ -6,7 +6,7 @@ router.post('/', verifyAccessToken, postCtrl.create)
 router.patch('/:id', verifyAccessToken, postCtrl.update)
 router.delete('/:id', verifyAccessToken, postCtrl.delete)
 
-router.get('/', verifyRefreshToken, postCtrl.findAll)
-router.get('/:id', verifyRefreshToken, postCtrl.findOne)
+router.get('/', verifyAccessToken, postCtrl.findAll)
+router.get('/:id', verifyAccessToken, postCtrl.findOne)
 
 module.exports = router
